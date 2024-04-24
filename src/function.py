c1a5ed061e12c9400288ff280e01cc51acb9e05b
nekono <91360587+nkte8@users.noreply.github.com>
2024-04-24 14:32:29 +0900

    ダウンロードが上手くいかないバグの修正

--- src/function.py
+++ src/function.py
@@ -7,17 +7,18 @@ def download(url: str, param: dict) -> None:
     param["quiet"] = True
     param["noplaylist"] = True
     param["overwrites"] = True
+    param["outtmpl"] = str(get_title(url=url) + ".%(ext)s")
     with YoutubeDL(param) as ydl:
-        param["outtmpl"] = get_title(ydl=ydl, url=url) + ".%(ext)s"
         ydl.download(url)
 
 
-def get_title(ydl: YoutubeDL, url: str) -> str:
-    info_dict = ydl.extract_info(url, download=False)
-    ydl.list_formats(info_dict)
-    return (
-        info_dict.get("title").replace("/", "-").replace("\\", "-")
-        + " ["
-        + info_dict["id"]
-        + "]"
-    )
+def get_title(url: str) -> str:
+    with YoutubeDL({"noplaylist": True}) as ydl:
+        info_dict = ydl.extract_info(url, download=False)
+        ydl.list_formats(info_dict)
+        return (
+            info_dict.get("title").replace("/", "-").replace("\\", "-")
+            + " ["
+            + info_dict["id"]
+            + "]"
+        )
